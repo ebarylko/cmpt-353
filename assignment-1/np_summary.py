Fixed average_precipitation_in_cities to divide by the total number of observations in a year

assignment-1/np_summary.py
@@ -31,24 +31,25 @@ def average_monthly_precipitation(precipitation, observations):
     )
 
 
-def test_average_precipitation_in_cites(cities):
+def average_precipitation_in_cites(cities, observations):
     """
     Args:
         cities: a series of collections containing the monthly precipitations across a full year for multiple cities
+        observations: a series of collections with the number of observations per-month for each city
 
     Returns: returns a collection of the average precipitations for each city
 
     """
-    return tz.thread_first(
+    return tz.thread_last(
         cities,
-        (np.sum, 1),
+        (map, np.sum),
+        (zip, np.sum(observations, 1)),
+        (map, lambda pair: pair[1] / pair[0]),
         list,
-        (np.divide, 12)
     )
 
 
 print(row_of_city_with_lowest_precipitation(totals))
 print(average_monthly_precipitation(totals, counts))
-# print(counts)
-# print(totals)
+print(average_precipitation_in_cites(totals, counts))
 

assignment-1/test_np_summary.py
@@ -4,7 +4,6 @@ import np_summary as nps
 
 sample_precipitation = np.array([[1, 2, 3], [3, 4, 5]])
 sample_counts = np.array([[6, 9, 2], [4, 2, 5]])
-larger_sample = np.array([[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]])
 
 
 def test_row_of_city_with_lowest_precipitation():
@@ -16,4 +15,4 @@ def test_average_monthly_precipitation():
 
 
 def test_average_precipitation_in_cites():
-    ts.assert_equal(nps.test_average_precipitation_in_cites(larger_sample), np.array([1, 2]))
+    ts.assert_equal(nps.average_precipitation_in_cites(sample_precipitation, sample_counts), np.array([6 / 17, 12 / 11]))