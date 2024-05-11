30c6c37abb9637377c5170d724b76768d38f7b8b
Eitan Barylko <eitan@barylko.com>
2024-05-10 21:51:08 -0700

    Added test for finding the city with the lowest precipitation

--- assignment-1/pd_summary.py
+++ assignment-1/pd_summary.py
@@ -1,4 +1,31 @@
 import pandas as pd
 import numpy as np
+import functools as ft
+import toolz as tz
+import operator as op
+
+
+def thread_last(val, *forms):
+    def evalform_back(val, form):
+        if callable(form):
+            return form(val)
+        if isinstance(form, tuple):
+            func, args = form[0], form[1:]
+            args = args + (val,)
+            return func(*args)
+    return ft.reduce(evalform_back, forms, val)
 
 totals = pd.read_csv('e1/totals.csv').set_index(keys=['name'])
+
+
+def city_with_lowest_precipitation(cities):
+
+    """
+    Args:
+        cities: a series of collections where each one records the monthly precipitation for a specific city
+
+    Returns: the city with the lowest annual precipitation
+
+    """
+    return tz.compose(op.methodcaller("idxmin"),
+                      op.methodcaller("apply", np.sum, axis=1))(cities)
--- assignment-1/test_pd_summary.py
+++ assignment-1/test_pd_summary.py
@@ -1,10 +1,13 @@
 import pd_summary as pds
 import pandas as pd
 import numpy as np
+import numpy.testing as ts
 
 sample_cities = pd.DataFrame(np.array([[1, 2, 3], [3, 4, 5]]), index=["a", "b"])
 
 print(sample_cities)
 
-# def test_city_with_lowest_precipitation():
-#     assert pds.city_with_lowest_precipitation()
+
+def test_city_with_lowest_precipitation():
+    assert pds.city_with_lowest_precipitation(sample_cities) == "a"
+
