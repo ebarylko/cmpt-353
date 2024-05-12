ba2ae5fd908a0c55f67c06d0f1d9c4446cad2533
Eitan Barylko <eitan@barylko.com>
2024-05-12 14:39:11 -0700

    Added additional tests which uses the data given for the assignment

--- assignment-1/test_np_summary.py
+++ assignment-1/test_np_summary.py
@@ -3,23 +3,37 @@ import numpy.testing as ts
 import np_summary as nps
 import itertools as it
 
+data = np.load('e1/monthdata.npz')
+totals = data['totals']
+counts = data['counts']
 sample_precipitation = np.array([[1, 2, 3], [3, 4, 5]])
-sample_counts = np.array([[6, 9, 2], [4, 2, 5]])
-larger_sample = np.array([list(range(12)), list(it.repeat(1, 12))])
 
 
 def test_row_of_city_with_lowest_precipitation():
     assert nps.row_of_city_with_lowest_precipitation(sample_precipitation) == 0
+    assert nps.row_of_city_with_lowest_precipitation(totals) == 8
+
+
+sample_counts = np.array([[6, 9, 2], [4, 2, 5]])
 
 
 def test_average_monthly_precipitation():
     ts.assert_equal(nps.average_monthly_precipitation(sample_precipitation, sample_counts),
                     np.array([0.4, 6 / 11, 8 / 7]))
+    ts.assert_allclose(nps.average_monthly_precipitation(totals, counts),
+                       np.array([27.77978339, 30.42629482, 29.41007194, 17.96654275, 21.34456929, 20.69498069,
+                                 24.97718631, 19.85661765, 24.06563707, 44.68441065, 34.61568627, 32.36679537]))
 
 
 def test_average_precipitation_in_cites():
     ts.assert_equal(nps.average_precipitation_in_cites(sample_precipitation, sample_counts),
                     np.array([6 / 17, 12 / 11]))
+    ts.assert_allclose(nps.average_precipitation_in_cites(totals, counts),
+                       np.array([47.77859779, 14.33333333, 39.91232877, 41.44505495, 23.4099723,  23.68144044,
+                                 17.52197802, 36.52222222,  6.15426997]))
+
+
+larger_sample = np.array([list(range(12)), list(it.repeat(1, 12))])
 
 
 def test_precipitation_quarters_for_each_city():
--- assignment-1/test_pd_summary.py
+++ assignment-1/test_pd_summary.py
@@ -13,10 +13,14 @@ def test_city_with_lowest_precipitation():
     assert pds.city_with_lowest_precipitation(totals) == "YELLOWKNIFE A"
 
 
-expected_monthly_precipitation = pd.Series(np.array([4 / 3, 3 / 2, 8 / 5]), index=["Jan", "Feb", "Mar"])
+expected_small_sample_averages = pd.Series(np.array([4 / 3, 3 / 2, 8 / 5]), index=["Jan", "Feb", "Mar"])
 all_observations = pd.read_csv('e1/counts.csv').set_index(keys=["name"])
+actual_monthly_averages = np.array([27.779783, 30.426295, 29.410072, 17.966543, 21.344569, 20.694981, 24.977186, 19.856618, 24.065637, 44.684411, 34.615686, 32.366795])
+all_months = ["2016-01", "2016-02", "2016-03", "2016-04", "2016-05", "2016-06", "2016-07", "2016-08", "2016-09", "2016-10", "2016-11", "2016-12"]
+expected_large_sample_averages = pd.Series(actual_monthly_averages, index=all_months)
 
 
 def test_average_monthly_precipitation():
     pdt.assert_series_equal(pds.average_monthly_precipitation(sample_cities, sample_observations),
-                            expected_monthly_precipitation)
+                            expected_small_sample_averages)
+    pdt.assert_series_equal(pds.average_monthly_precipitation(totals, all_observations), expected_large_sample_averages)
