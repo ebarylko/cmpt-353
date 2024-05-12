e9efedd0bbb81506a32345a9df320c7d1044544f
Eitan Barylko <eitan@barylko.com>
2024-05-12 13:21:54 -0700

    Refactored the functions in pd_summary.py to not use the functions from Toolz

--- assignment-1/pd_summary.py
+++ assignment-1/pd_summary.py
@@ -5,27 +5,9 @@ import toolz as tz
 import operator as op
 
 
-def thread_last(val, *forms):
-    def evalform_back(val, form):
-        if callable(form):
-            return form(val)
-        if isinstance(form, tuple):
-            func, args = form[0], form[1:]
-            args = args + (val,)
-            return func(*args)
-
-    return ft.reduce(evalform_back, forms, val)
-
-
 totals = pd.read_csv('e1/totals.csv').set_index(keys=['name'])
 
-set_rows = op.methodcaller("set_index", keys=['name'])
-
-all_observations = tz.thread_first(
-    'e1/counts.csv',
-    pd.read_csv,
-    set_rows
-)
+all_observations = pd.read_csv('e1/counts.csv').set_index(keys=["name"])
 
 
 def city_with_lowest_precipitation(cities):
@@ -36,8 +18,7 @@ def city_with_lowest_precipitation(cities):
     Returns: the city with the lowest annual precipitation
 
     """
-    return tz.compose(op.methodcaller("idxmin"),
-                      op.methodcaller("apply", np.sum, axis=1))(cities)
+    return cities.apply(np.sum, axis=1).idxmin()
 
 
 def average_monthly_precipitation(cities, observations):
@@ -48,8 +29,10 @@ def average_monthly_precipitation(cities, observations):
 
     Returns: the average precipitation in all months of the year
     """
-    return (cities.pipe(op.methodcaller("apply", np.sum, axis=0))
-            .pipe(np.divide, observations.apply(np.sum, axis=0)))
+    def sum_rows(rows):
+        return rows.apply(np.sum, 0)
+
+    return cities.pipe(sum_rows).pipe(np.divide, sum_rows(observations))
 
 
 print(city_with_lowest_precipitation(totals))
--- assignment-1/test_pd_summary.py
+++ assignment-1/test_pd_summary.py
@@ -5,13 +5,16 @@ import numpy as np
 
 sample_cities = pd.DataFrame(np.array([[1, 2, 3], [3, 4, 5]]), index=["a", "b"], columns=["Jan", "Feb", "Mar"])
 sample_observations = pd.DataFrame([[1, 1, 1], [2, 3, 4]], columns=["Jan", "Feb", "Mar"])
+totals = pd.read_csv('e1/totals.csv').set_index(keys=['name'])
 
 
 def test_city_with_lowest_precipitation():
     assert pds.city_with_lowest_precipitation(sample_cities) == "a"
+    assert pds.city_with_lowest_precipitation(totals) == "YELLOWKNIFE A"
 
 
 expected_monthly_precipitation = pd.Series(np.array([4 / 3, 3 / 2, 8 / 5]), index=["Jan", "Feb", "Mar"])
+all_observations = pd.read_csv('e1/counts.csv').set_index(keys=["name"])
 
 
 def test_average_monthly_precipitation():
