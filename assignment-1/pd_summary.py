Corrected the test for getting the average precipitation using pandas

assignment-1/pd_summary.py
@@ -35,19 +35,18 @@ def average_monthly_precipitation(cities, observations):
     return cities.pipe(sum_rows).pipe(np.divide, sum_rows(observations))
 
 
-def average_annual_precipitations(cities, observations):
+def average_annual_precipitations(cities, observations) -> pd.Series:
     """
     Args:
         cities: a series of collections where each one contains the monthly precipitations for a city
         observations: a series of collections where each one contains the number of observations per month for a city
 
     Returns: the yearly annual precipitations in all the cities
-
     """
-    def sum_columns(columns):
+    def sum_rows(columns):
         return columns.apply(np.sum, 'columns')
 
-    return sum_columns(cities) / sum_columns(observations)
+    return sum_rows(cities) / sum_rows(observations)
 
 
 print(city_with_lowest_precipitation(totals))

assignment-1/test_pd_summary.py
@@ -46,15 +46,11 @@ cities = ["BURNABY SIMON FRASER U",
           "VANCOUVER INTL A",
           "YELLOWKNIFE A"]
 
-# expected_average_precipitations = pd.Series(yearly_averages, index=cities)
-# print(expected_average_precipitations)
-# wx = pd.DataFrame(expected_average_precipitations)
-# wx = pd.DataFrame(expected_average_precipitations, columns=["name"]).set_index("name")
+expected_average_precipitations = pd.Series(yearly_averages, index=cities, name="name")
 
-# print(wx)
 
 def test_average_annual_precipitations():
     pdt.assert_series_equal(pds.average_annual_precipitations(sample_cities, sample_observations),
                             pd.Series(np.array([2, 4 / 3]), index=["a", "b"]))
-    # pdt.assert_series_equal(pds.average_annual_precipitations(totals, all_observations),
-    #                         expected_average_precipitations)
+    pdt.assert_series_equal(pds.average_annual_precipitations(totals, all_observations),
+                            expected_average_precipitations, check_names=False)