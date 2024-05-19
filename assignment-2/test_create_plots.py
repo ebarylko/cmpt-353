80d050e2328132cac235210d963995922edead97
Eitan Barylko <eitan@barylko.com>
2024-05-18 21:59:53 -0700

    Made a small change

--- assignment-2/test_create_plots.py
+++ assignment-2/test_create_plots.py
@@ -2,9 +2,6 @@ import pandas as pd
 import create_plots as cp
 import numpy as np
 import numpy.testing as npt
-# import matplotlib as mb
-# import matplotlib.pyplot as plt
-import os
 
 expected_views = [10, 2, 1]
 
