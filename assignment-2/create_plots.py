fec3539bb3233fd0fdfa410bf09aa02e7bcd71a6
Eitan Barylko <eitan@barylko.com>
2024-05-19 00:29:44 -0700

    Added a test for finding pages that are in both files

--- assignment-2/create_plots.py
+++ assignment-2/create_plots.py
@@ -4,6 +4,11 @@ import matplotlib.pyplot as plt
 import os
 
 
+def pages_and_views(file_name):
+    return pd.read_csv(file_name, sep=' ', header=None, index_col=1,
+                       names=['lang', 'page', 'views', 'bytes'])
+
+
 def sort_views_descending(file_name):
     """
     Args:
@@ -13,17 +18,34 @@ def sort_views_descending(file_name):
     Returns: a collection containing the number of times each page has been
     viewed sorting decreasingly
     """
-    pages_and_views = pd.read_csv(file_name, sep=' ', header=None, index_col=1,
-                                  names=['lang', 'page', 'views', 'bytes'])
-    return pages_and_views['views'].sort_values(ascending=False).to_numpy()
+    return pages_and_views(file_name)['views'].sort_values(ascending=False).to_numpy()
+
+
+def pages_common_to_both_files(file_1, file_2):
+    """
+    Args:
+        file_1: a file containing the pages viewed in the first hour
+        file_2: a file containing the pages viewed in the second hour
+
+    Returns: the pages which appear in both files
+    """
+    pages_in_file_1 = pages_and_views(file_1)
+    pages_in_file_2 = pages_and_views(file_2)
+    return pd.merge(pages_in_file_1, pages_in_file_2, on="page")
 
 
 if not os.getenv("TESTING"):
     plt.figure(figsize=(10, 5))
     plt.subplot(1, 2, 1)
-    first_hour_views = sort_views_descending(sys.argv[1])
-    plt.plot(first_hour_views)
-    plt.show()
-
-
+    # first_hour_views = sort_views_descending(sys.argv[1])
+    # plt.plot(first_hour_views)
+    # plt.show()
+    plt.subplot(1, 2, 2)
+    pages_in_both_hours = pages_common_to_both_files
+    # first_hour_views_and_pages = pages_and_views(sys.argv[1])
+    # snd_hour_views_and_pages = pages_and_views(sys.argv[2])
+    # ex = pd.merge(first_hour_views_and_pages, snd_hour_views_and_pages, on="page")
+    # print(ex)
+    # plt.plot(first_hour_views, snd_hour_views)
+    # plt.show()
 
--- assignment-2/test_create_plots.py
+++ assignment-2/test_create_plots.py
@@ -2,9 +2,19 @@ import pandas as pd
 import create_plots as cp
 import numpy as np
 import numpy.testing as npt
+import pandas.testing as pdt
 
 expected_views = [10, 2, 1]
 
 
 def test_sort_pages_by_view():
-    npt.assert_equal(cp.sort_views_descending("sample_plots_sample.txt"), expected_views)
+    npt.assert_equal(cp.sort_views_descending("sample_plots_sample_1.txt"), expected_views)
+
+
+expected_pages = pd.DataFrame({"page": ["Athol_Moffitt"], "lang_x": "en", "views_x": [1], "bytes_x": 0,
+                               "lang_y": "en", "views_y": [3], "bytes_y": 0}).set_index("page")
+
+
+def test_pages_common_to_both_files():
+    pdt.assert_frame_equal(cp.pages_common_to_both_files("sample_plots_sample_1.txt", "sample_plots_sample_2.txt"),
+                           expected_pages)
