28bc94c462903f80616de3af262baf2bde9b5951
Eitan Barylko <eitan@barylko.com>
2024-05-18 20:07:10 -0700

    Added a test for ordering by the number of views descending

--- assignment-2/create_plots.py
+++ assignment-2/create_plots.py
@@ -1,10 +1,32 @@
 import sys
 import pandas as pd
+import matplotlib.pyplot as plt
 
-name_1 = sys.argv[1]
-# name_2 = sys.argv[2]
+# file_name_1 = sys.argv[1]
+# file_name_2 = sys.argv[2]
+#
+# fst_hour_views = pd.read_csv(file_name_1, sep=' ', header=None, index_col=1,
+#                      names=['lang', 'page', 'views', 'bytes'])
 
-file_1 = pd.read_csv(name_1, sep=' ', header=None, index_col=1,
-                     names=['lang', 'page', 'views', 'bytes'])
+# print(fst_hour_views)
 
-print(file_1)
+# snd_hour_views = pd.read_csv(file_name_2, sep=' ', header=None, index_col=1,
+#                              names=['lang', 'page', 'views', 'bytes'])
+#
+# print(snd_hour_views['views'])
+
+plt.figure(figsize=(10, 5))
+plt.subplot(1, 2, 1)
+plt.subplot(1, 2, 2)
+
+
+def sort_pages_by_view(views):
+    """
+    Args:
+        views: a collection of key value pairs mapping a page to the number of
+        times it has been viewed in the hour
+
+    Returns: a collection containing the number of times each page has been
+    viewed sorting decreasingly
+    """
+    return views.sort_values(ascending=False).array
---
+++ assignment-2/test_create_plots.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import create_plots as cp
+import numpy as np
+import numpy.testing as npt
+
+sample_views = pd.Series(np.array([4, 1, 9, 12]), index=['a', 'b', 'c', 'd'])
+
+
+def test_sort_pages_by_view():
+    npt.assert_equal(cp.sort_pages_by_view(sample_views), [12, 9, 4, 1])
