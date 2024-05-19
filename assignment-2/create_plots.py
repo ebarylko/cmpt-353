04b5f916a86d45465bb4e404f8bc0d921c91a8ce
Eitan Barylko <eitan@barylko.com>
2024-05-18 21:28:11 -0700

    Corrected the test

--- assignment-2/create_plots.py
+++ assignment-2/create_plots.py
@@ -1,45 +1,29 @@
 import sys
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
 
 
-def get_file_name(num):
+def sort_views_descending(file_name):
     """
     Args:
-        num: either 1 or 2, representing the first or second file respectively
-    Returns: the first file if num is 1, the second file otherwise
-    """
-    return sys.argv[num]
-
-
-file_name_1 = sys.argv[1]
-
-
-fst_hour_views = pd.read_csv(file_name_1, sep=' ', header=None, index_col=1,
-                     names=['lang', 'page', 'views', 'bytes'])
-
-# print(fst_hour_views)
-
-# snd_hour_views = pd.read_csv(file_name_2, sep=' ', header=None, index_col=1,
-#                              names=['lang', 'page', 'views', 'bytes'])
-#
-
-plt.figure(figsize=(10, 5))
-
-plt.subplot(1, 2, 1)
-plt.plot(fst_hour_views['views'].sort_values(ascending=False).array)
-plt.show()
-
-plt.subplot(1, 2, 2)
-
-
-def sort_views_descending(views):
-    """
-    Args:
-        views: a collection of key value pairs mapping a page to the number of
-        times it has been viewed in the hour
+        file_name: the name of a file where each line contains the name of a
+        Wikipedia page and the number of times it has been viewed in the current hour
 
     Returns: a collection containing the number of times each page has been
     viewed sorting decreasingly
     """
-    return views.sort_values(ascending=False).array
+    pages_and_views = pd.read_csv(file_name, sep=' ', header=None, index_col=1,
+                                  names=['lang', 'page', 'views', 'bytes'])
+    return pages_and_views['views'].sort_values(ascending=False).to_numpy()
+
+
+if not os.getenv("TESTING"):
+    plt.figure(figsize=(10, 5))
+    plt.subplot(1, 2, 1)
+    first_hour_views = sort_views_descending(sys.argv[1])
+    plt.plot(first_hour_views)
+    plt.show()
+
+
+
--- assignment-2/test_create_plots.py
+++ assignment-2/test_create_plots.py
@@ -2,9 +2,12 @@ import pandas as pd
 import create_plots as cp
 import numpy as np
 import numpy.testing as npt
+# import matplotlib as mb
+# import matplotlib.pyplot as plt
+import os
 
-sample_views = pd.Series(np.array([4, 1, 9, 12]), index=['a', 'b', 'c', 'd'])
+expected_views = [10, 2, 1]
 
 
 def test_sort_pages_by_view():
-    npt.assert_equal(cp.sort_views_descending(sample_views), [12, 9, 4, 1])
+    npt.assert_equal(cp.sort_views_descending("sample_plots_sample.txt"), expected_views)
