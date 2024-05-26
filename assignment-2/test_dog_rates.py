4141dd1cb00bc409badefbb3eab705f90cb2cbbb
Eitan Barylko <eitan@barylko.com>
2024-05-26 11:41:41 -0700

    Corrected test_dates_and_ratings_of_valid_tweets

--- assignment-2/test_dog_rates.py
+++ assignment-2/test_dog_rates.py
@@ -24,26 +24,17 @@ def test_tweet_to_rating():
                                      "If not, he can try again. 14/10 they… https://t.co/oTWddg2972"), 14)
 
 
-def dates_and_ratings_of_tweets(tweets):
+def dates_and_ratings_of_valid_tweets(tweets):
     """
     Args:
         tweets: a DataFrame where each row contains the date a tweet was sent, the user who made
         the tweet, and the content of the tweet
 
-    Returns: a DataFrame where each row has a date and rating of a tweet which
-    contains a rating of the form x/10, where x is in [0, 25]
+    Returns: a DataFrame where each row has a date and rating of a tweet
+    corresponding to that date which contains a rating of the form x/10,
+    where x is in [0, 25]
     """
-
-    def validate_ratings(rating):
-        return np.nan if rating > 25 else rating
-
-    def remove_invalid_ratings(tweet_coll):
-        tweets_cpy = tweet_coll.copy()
-        tweets_cpy['rating'] = tweets_cpy['rating'].map(validate_ratings)
-        return tweets_cpy.dropna()[['rating', 'created_at']].set_index('rating')
-
-    tweets_with_ratings = tweets.assign(rating=tweets['text'].apply(extract_rating))
-    return remove_invalid_ratings(tweets_with_ratings)
+    return tweets.assign(rating=tweets['text'].apply(tweet_to_rating)).dropna()
 
 
 sample_tweets = pd.DataFrame({'id': [1, 2, 3],
@@ -52,12 +43,15 @@ sample_tweets = pd.DataFrame({'id': [1, 2, 3],
                                              pd.Timestamp('2018-05-13 20:36:44')],
                               'text': ['Hi there', 'Perfect. 10/10', 'Lol. 19/10']})
 
-expected_dates_and_ratings = pd.DataFrame({'rating': [10, 19],
+expected_dates_and_ratings = pd.DataFrame({'id': [2, 3],
                                            'created_at': [pd.Timestamp('2018-05-12 20:36:44'),
-                                                          pd.Timestamp('2018-05-13 20:36:44')]}).set_index('rating')
+                                                          pd.Timestamp('2018-05-13 20:36:44')],
+                                           'text': ['Perfect. 10/10', 'Lol. 19/10'],
+                                           'rating': [10, 19],
+                                           }).set_index('rating')
 
 
-def test_dates_and_ratings_of_tweets():
-    pdt.assert_frame_equal(dates_and_ratings_of_tweets(sample_tweets),
+def test_dates_and_ratings_of_valid_tweets():
+    pdt.assert_frame_equal(dates_and_ratings_of_valid_tweets(sample_tweets).set_index('rating'),
                            expected_dates_and_ratings,
                            check_index_type=False)
