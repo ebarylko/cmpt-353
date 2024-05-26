dfac56cc586c1af5b70fd3aceeb166efaee1a1ad
Eitan Barylko <eitan@barylko.com>
2024-05-26 13:25:58 -0700

    Added plot of data using lowess filtering

--- assignment-3/smooth_temperature.py
+++ assignment-3/smooth_temperature.py
@@ -1,10 +1,13 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+from statsmodels.nonparametric.smoothers_lowess import lowess
+
+data = pd.read_csv('sysinfo.csv', header=0, parse_dates=[0])
 
 
-data = pd.read_csv('sysinfo.csv', header=0)
-
-print(data)
 plt.figure(figsize=(12, 4))
+plt.xticks(rotation=25)
 plt.plot(data['timestamp'], data['temperature'], 'b.', alpha=0.5)
+smoothed_data = lowess(data['temperature'], data['timestamp'], frac=0.17)[:, 1]
+plt.plot(data['timestamp'], smoothed_data, 'r-')
 plt.show()
