b5d677209bb9c022a58cdf35a4c3dbdcf411b68d
Eitan Barylko <eitan@barylko.com>
2024-05-26 15:07:13 -0700

    Plotted the result of using the Lowess smoothing

--- assignment-3/smooth_temperature.py
+++ assignment-3/smooth_temperature.py
@@ -3,11 +3,25 @@ import pandas as pd
 from statsmodels.nonparametric.smoothers_lowess import lowess
 
 data = pd.read_csv('sysinfo.csv', header=0, parse_dates=[0])
+kalman_data = data[['temperature', 'cpu_percent', 'sys_load_1', 'fan_rpm']]
 
 
-plt.figure(figsize=(12, 4))
-plt.xticks(rotation=25)
-plt.plot(data['timestamp'], data['temperature'], 'b.', alpha=0.5)
-smoothed_data = lowess(data['temperature'], data['timestamp'], frac=0.17)[:, 1]
-plt.plot(data['timestamp'], smoothed_data, 'r-')
-plt.show()
+def print_data_lowess_and_kalman(df: pd.DataFrame):
+    """
+    @param df: a DataFrame with information about the temperature, cpu usage, load on the system, the fan rotations
+    per minute, and the date the observation was taken
+    @return: plots the temperature compared against the moment the observation was taken in three different ways:
+    using the normal data, using the data after using Lowess smoothing, and using the Kalman filter
+    """
+    plt.figure(figsize=(12, 4))
+    plt.xticks(rotation=25)
+    temperatures = df['temperature']
+    dates = df['timestamp']
+    plt.plot(dates, temperatures, 'b.', alpha=0.5)
+    smoothed_data = lowess(temperatures, dates, frac=0.17)[:, 1]
+    plt.plot(dates, smoothed_data, 'r-')
+    plt.legend(['Measurements', 'Lowess'])
+    plt.savefig('data.png')
+
+
+print_data_lowess_and_kalman(data)
