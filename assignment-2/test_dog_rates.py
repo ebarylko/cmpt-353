7742679a3f4ba873015e211a0575ad9def4d2672
Eitan Barylko <eitan@barylko.com>
2024-05-26 11:14:21 -0700

    Changed test for extract_rating

--- assignment-2/test_dog_rates.py
+++ assignment-2/test_dog_rates.py
@@ -3,28 +3,25 @@ import numpy.testing as npt
 import re
 import pandas.testing as pdt
 import pandas as pd
+import numbers
 
 
-def extract_rating(tweet):
+def tweet_to_rating(tweet):
     """
     Args:
         tweet: a string that optionally contains a rating of the form x/10, where x is a positive integer
 
     Returns: NAN if the tweet has no rating, otherwise returns the numerical value of the rating
     """
-
-    def process_rating(rating):
-        num, _ = map(float, rating.split('/'))
-        return num
-
-    match = re.search(r'(\d+(\.\d+)?)/10', tweet)
-    return np.nan if not match else process_rating(match.group())
+    match = re.search(r'(\d+(?:\.\d+)?)/10', tweet)
+    value = match and float(match.groups()[0])
+    if isinstance(value, numbers.Number) and value <= 25.0: return value
 
 
-def test_extract_rating():
-    npt.assert_equal(extract_rating("@Wibblywobblykid no dollar sign just the whole number should work"), np.nan)
-    npt.assert_equal(extract_rating("This is Oakley. He picked you some flowers. Hopes they’re your favorite color. "
-                                    "If not, he can try again. 14/10 they… https://t.co/oTWddg2972"), 14)
+def test_tweet_to_rating():
+    npt.assert_equal(tweet_to_rating("@Wibblywobblykid no dollar sign just the whole number should work"), None)
+    npt.assert_equal(tweet_to_rating("This is Oakley. He picked you some flowers. Hopes they’re your favorite color. "
+                                     "If not, he can try again. 14/10 they… https://t.co/oTWddg2972"), 14)
 
 
 def dates_and_ratings_of_tweets(tweets):
@@ -36,6 +33,7 @@ def dates_and_ratings_of_tweets(tweets):
     Returns: a DataFrame where each row has a date and rating of a tweet which
     contains a rating of the form x/10, where x is in [0, 25]
     """
+
     def validate_ratings(rating):
         return np.nan if rating > 25 else rating
 
@@ -57,8 +55,8 @@ sample_tweets = pd.DataFrame({'id': [1, 2, 3],
 expected_dates_and_ratings = pd.DataFrame({'rating': [10, 19],
                                            'created_at': [pd.Timestamp('2018-05-12 20:36:44'),
                                                           pd.Timestamp('2018-05-13 20:36:44')]}).set_index('rating')
-print(expected_dates_and_ratings)
-print(dates_and_ratings_of_tweets(sample_tweets))
+
+
 def test_dates_and_ratings_of_tweets():
     pdt.assert_frame_equal(dates_and_ratings_of_tweets(sample_tweets),
                            expected_dates_and_ratings,
