fed88b336040954a3b4f8b68ba79f57eda5d4dbd
Eitan Barylko <eitan@barylko.com>
2024-05-28 11:26:10 -0700

    Updated test for get_lat_lon_date

--- assignment-3/calc_distance.py
+++ assignment-3/calc_distance.py
@@ -1,2 +1,20 @@
 import xml.etree.ElementTree as et
+from datetime import datetime
 
+namespaces = {"pre": "http://www.topografix.com/GPX/1/0"}
+
+
+def get_lat_lon_and_date(file_name):
+    """
+    @param file_name: the name of a xml file containing a collection of observations where each one has a
+     latitude, longitude, and the date
+    @return: returns three collections where the first is the latitudes of all the observations, the second is
+    the longitudes of all the observations, and the third is the date of all the observations
+    """
+    all_triplets = []
+    observations = et.parse(file_name).getroot().iter('{http://www.topografix.com/GPX/1/0}trkpt')
+    for observation in observations:
+        all_triplets.append([float(observation.get("lat")),
+                             float(observation.get("lon")),
+                             observation.find('{http://www.topografix.com/GPX/1/0}time').text])
+    return list(zip(*all_triplets))
--- assignment-3/test_calc_distance.py
+++ assignment-3/test_calc_distance.py
@@ -1,7 +1,9 @@
 import calc_distance as cd
 import pandas as pd
 
-def test_get_all_lat_long_time_triplets():
-    assert cd.get_all_lat_long_triplets("ex.gpx") == [{"lat": 1, "lon": 2, "time": "2022-02-14"},
-                                                      {"lat": 3, "lon": 4, "time": "2020-03-14"}]
+cd.get_lat_lon_and_date("ex.gpx")
+
+
+def test_get_lat_lon_and_date():
+    assert cd.get_lat_lon_and_date("ex.gpx") == [(1, 3), (2, 4), ("2022-02-14", "2020-03-04")]
 
