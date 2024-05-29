c93ece204c32f176ae5e3da944ca217d1240606a
Eitan Barylko <eitan@barylko.com>
2024-05-29 08:41:54 -0700

    Added test for combining the latitude and longitude readings with the compass readings

--- assignment-3/calc_distance.py
+++ assignment-3/calc_distance.py
@@ -9,8 +9,10 @@ def get_lat_lon_and_date(file_name: str) -> pd.DataFrame:
     """
     @param file_name: the name of a xml file containing a collection of observations where each one has a
      latitude, longitude, and the date
-    @return: returns a DataFrame where each row contains the latitudes, longitudes, and dates of all the observations
+    @return: returns a latitude, longitude, and date of each observation as different rows within a
+    DataFrame
     """
+
     def lat_lon_date(observation):
         """
         @param observation: an observation containing the latitude, longitude, and date of observation
@@ -23,7 +25,25 @@ def get_lat_lon_and_date(file_name: str) -> pd.DataFrame:
     observations = et.parse(file_name).getroot().iter('{http://www.topografix.com/GPX/1/0}trkpt')
     lat_lon_and_dates_triples = map(lat_lon_date, observations)
     lat, lon, dates = list(zip(*lat_lon_and_dates_triples))
-    return pd.DataFrame({"lat": lat, "lon": lon, "date": pd.to_datetime(dates)})
+    return pd.DataFrame({"lat": lat, "lon": lon, "date": pd.to_datetime(dates, utc=True)})
+
+
+def read_compass_readings(file_name) -> pd.DataFrame:
+    return pd.read_csv(file_name)
+
+
+def combine_lat_lon_and_date_and_compass_readings(lat_lon_date: pd.DataFrame,
+                                                  compass_readings: pd.DataFrame) -> pd.DataFrame:
+    """
+    @param lat_lon_date: a DataFrame containing the latitude, longitude, and date of an observation
+    in each row
+    @param compass_readings: a DataFrame where each row is an observation containing the date, x-coordinate,
+    y-coordinate, z-coordinate, acceleration in the x, y, and z dimensions
+    @return: a subset of the two DataFrames joined by the date, where each row contains the latitude,
+     longitude, x-coordinate, y-coordinate, and date of an observation
+    """
+    return compass_readings.rename(columns={"datetime": "date"}).merge(lat_lon_date, on="date")[
+        ["lat", "lon", "date", "Bx", "By"]]
 
 
 def distance(df: pd.DataFrame) -> int:
--- assignment-3/test_calc_distance.py
+++ assignment-3/test_calc_distance.py
@@ -4,16 +4,32 @@ import pandas.testing as pdt
 
 cd.get_lat_lon_and_date("ex.gpx")
 
-expected = pd.DataFrame({"lat": [1., 3.],
-                         "lon": [2., 4.],
+expected = pd.DataFrame({"lat": [1, 3],
+                         "lon": [2, 4],
                          "date": [pd.to_datetime("2022-02-14"),
                                   pd.to_datetime("2020-03-04")]})
 
 
 def test_get_lat_lon_and_date():
-    pdt.assert_frame_equal(expected, cd.get_lat_lon_and_date("ex.gpx"))
+    pdt.assert_frame_equal(expected, cd.get_lat_lon_and_date("ex.gpx"), check_dtype=False)
 
-# sample = [(1, 3), (2, 4), ("2022-02-14", "2020-03-04")]
 
-# def test_lat_lon_and_date_to_pd():
-#     assert lat_lon_and_date_to_pd(expected)
+expected_data = pd.DataFrame({"lat": [1], "lon": [2], "date": [pd.to_datetime("2022-04-01")], "Bx": [3], "By": [4]})
+sample_lat_lon_date = pd.DataFrame({"lat": [1, 2],
+                                    "lon": [2, 3],
+                                    "date": [pd.to_datetime("2022-04-01"), pd.to_datetime("2022-07-01")]})
+
+sample_compass_readings = pd.DataFrame({"Bx": [3], "By": [4], "datetime": [pd.to_datetime("2022-04-01")]})
+
+
+def test_combine_lat_lon_and_date_and_compass_readings():
+    pdt.assert_frame_equal(expected_data,
+                           cd.combine_lat_lon_and_date_and_compass_readings(sample_lat_lon_date,
+                                                                            sample_compass_readings))
+
+sample_readings = pd.DataFrame({
+    'lat': [49.28, 49.26, 49.26],
+    'lon': [123.00, 123.10, 123.05],
+    "Bx": [1, 2, 3, 4],
+    "BY": [1, 2, 3, 4]})
+def test_distance():
