f29a6b6912c755357fde8a8bf7b36c2a4f3166c2
Eitan Barylko <eitan@barylko.com>
2024-05-30 22:36:02 -0700

    Modified the first part of the assignment to include more functions which describe the data being plotted on the graph

--- assignment-3/smooth_temperature.py
+++ assignment-3/smooth_temperature.py
@@ -3,22 +3,17 @@ import pandas as pd
 from statsmodels.nonparametric.smoothers_lowess import lowess
 from pykalman import KalmanFilter
 import numpy as np
-
-data = pd.read_csv('sysinfo.csv', header=0, parse_dates=[0])
+import os
+import sys
 
 
 def plot_kalman_filtering(df: pd.DataFrame):
     """
-    @param df:  with information about the temperature, cpu usage, load on the system, the fan rotations
+    @param df: DataFrame with information about the temperature, cpu usage, load on the system, the fan rotations
     per minute, and the date the observation was taken
-    @return: plots the results of the Kalman filtering on df
+    @return: plots the temperature compared against the date after applying the Kalman filtering on df
     """
     kalman_data = df[['temperature', 'cpu_percent', 'sys_load_1', 'fan_rpm']]
-    tmp_deviation = kalman_data['temperature'].var()
-    cpu_deviation = kalman_data['cpu_percent'].var()
-    load_deviation = kalman_data['sys_load_1'].var()
-    fan_deviation = kalman_data['fan_rpm'].var()
-
     initial_state = kalman_data.iloc[0]
     observation_covariance = np.diag([1.5, 0.1, 0.1, 10]) ** 2
     transition_covariance = np.diag([2, 20, 0.6, 100]) ** 2
@@ -30,16 +25,16 @@ def plot_kalman_filtering(df: pd.DataFrame):
         transition_covariance=transition_covariance,
         observation_covariance=observation_covariance
     )
+
     kf_smoothed, _ = kf.smooth(kalman_data)
     plt.plot(df['timestamp'], kf_smoothed[:, 0], 'g-')
 
 
-def print_data_lowess_and_kalman(df: pd.DataFrame):
+def plot_date_against_temperature(df: pd.DataFrame):
     """
-    @param df: a DataFrame with information about the temperature, cpu usage, load on the system, the fan rotations
-    per minute, and the date the observation was taken
-    @return: plots the temperature compared against the moment the observation was taken in three different ways:
-    using the normal data, using the data after using Lowess smoothing, and using the Kalman filter
+     @param df: a DataFrame with information about the temperature, cpu usage, load on the system, the fan rotations
+     per minute, and the date the observation was taken
+     @return: plots the temperature compared against the date of the observation
     """
     plt.figure(figsize=(12, 4))
     plt.xticks(rotation=25)
@@ -47,12 +42,24 @@ def print_data_lowess_and_kalman(df: pd.DataFrame):
     dates = df['timestamp']
     plt.plot(dates, temperatures, 'b.', alpha=0.5)
 
+
+def plot_lowess_filtered_data(df: pd.DataFrame):
+    """
+     @param df: a DataFrame with information about the temperature, cpu usage, load on the system, the fan rotations
+     per minute, and the date the observation was taken
+     @return: plots the temperature against the date of the observation after applying the LOWESS filter on the
+     temperature
+    """
+    temperatures = df['temperature']
+    dates = df['timestamp']
     smoothed_data = lowess(temperatures, dates, frac=0.06)[:, 1]
     plt.plot(dates, smoothed_data, 'r-')
 
-    plot_kalman_filtering(df)
+
+if not os.getenv("TESTING"):
+    data = pd.read_csv(sys.argv[1], header=0, parse_dates=[0])
+    plot_date_against_temperature(data)
+    plot_lowess_filtered_data(data)
+    plot_kalman_filtering(data)
     plt.legend(['Measurements', 'Lowess', 'Kalman'])
-    plt.savefig('data.png')
-
-
-print_data_lowess_and_kalman(data)
+    plt.savefig('cpu.svg')
