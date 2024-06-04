f54444427c67592bdd665612a1c769846cd87ed4
Eitan Barylko <eitan@barylko.com>
2024-06-04 08:12:03 -0700

    Added a test for distance

--- assignment-4/temperature_correlation.py
+++ assignment-4/temperature_correlation.py
@@ -1,6 +1,22 @@
 import os
 import sys
 import pandas as pd
+import math as m
+
+
+def read_weather_station_data(file_name):
+    """
+    @param file_name: the name of the file containing weather data
+    @return: a DataFrame containing the information present in file_name
+    """
+    return pd.read_json(file_name, lines=True)
+
+def read_city_data(file_name):
+    """
+    @param file_name: the name of the file containing information about cities
+    @return: a DataFrame containing the information present in file_name
+    """
+    return pd.read_csv(file_name, header=0)
 
 
 def remove_invalid_cities(cities: pd.DataFrame) -> pd.DataFrame:
@@ -10,3 +26,39 @@ def remove_invalid_cities(cities: pd.DataFrame) -> pd.DataFrame:
     @return: the cities which contain a valid population and area
     """
     return cities.dropna().query("area < 10000")
+
+
+def distance(fst_loc, snd_loc):
+    """
+    @param fst_loc: a collection containing a latitude and longitude
+    @param snd_loc: a collection containing a latitude and longitude
+    @return: the distance between fst_loc and snd_loc
+    """
+    def diff_between(x, y):
+        return m.radians(x - y) / 2
+
+    def sin_squared(x, y):
+        return m.sin(diff_between(y, x)) ** 2
+
+    earth_radius_in_meters = 6371000
+    fst_lat, fst_lon = fst_loc
+    snd_lat, snd_lon = snd_loc
+
+    fst_lat_in_rad = m.radians(fst_lat)
+    snd_lat_in_rad = m.radians(snd_lat)
+    tmp = m.sqrt(
+        sin_squared(fst_lat, snd_lat) + m.cos(fst_lat_in_rad) * m.cos(snd_lat_in_rad) * sin_squared(fst_lon, snd_lon)
+    )
+
+    dst_from_a_to_b = 2 * earth_radius_in_meters * m.asin(tmp)
+    return dst_from_a_to_b
+
+
+def closest_station(city: pd.Series, stations: pd.DataFrame) -> pd.Series:
+    """
+    @param city: a Series containing the name of the city, longitude, latitude, and other information
+    @param stations: a DataFrame where the rows contain the name of the station, longitude, latitude, elevation, average
+    temperature readings, and total number of observations
+    @return: the first station closest to the city
+    """
+    # return stations.assign(distance=)
--- assignment-4/test_temperature_correlation.py
+++ assignment-4/test_temperature_correlation.py
@@ -20,3 +20,31 @@ expected_cities = pd.DataFrame({"name": ["a"],
 def test_remove_invalid_cities():
     pdt.assert_frame_equal(tc.remove_invalid_cities(sample_cities), expected_cities,
                            check_dtype=False)
+
+
+sample_city = pd.Series({"name": "a",
+                         "population": 1,
+                         "area": 1,
+                         "latitude": 1,
+                         "longitude": 1})
+
+sample_stations = pd.DataFrame({"observations": [1, 1, 1],
+                                "avg_tmax": [1, 2, 3],
+                                "station": ["a1", "a2", "a3"],
+                                "latitude": [0, 0, 3],
+                                "longitude": [0, 2, 4],
+                                "elevation": [1, 3, 8]})
+
+expected_station = pd.DataFrame({"observations": [1],
+                                 "avg_tmax": [1],
+                                 "station": ["a1"],
+                                 "latitude": [0],
+                                 "longitude": [0],
+                                 "elevation": [1]})
+
+
+def test_distance():
+    assert round(tc.distance([1, 1], [0, 0])) == 157249
+
+def test_closest_station():
+    pdt.assert_frame_equal(tc.closest_station(sample_city, sample_stations), expected_station)
