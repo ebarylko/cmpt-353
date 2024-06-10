a1dbd35eaf45f1051ca8b1336ef4cb3f8947612d
Eitan Barylko <eitan@barylko.com>
2024-06-09 23:41:18 -0700

    Added a test for filtering out comments from 2012 and 2013

--- assignment-5/reddit_weekends.py
+++ assignment-5/reddit_weekends.py
@@ -1,4 +1,17 @@
 import pandas as pd
+import os
+import sys
+from scipy import stats
+
+
+def comments_only_in_2012_or_2013(comments: pd.DataFrame) -> pd.DataFrame:
+    """
+    @param comments: a DataFrame containing the date, subreddit name, and comment count in each row
+    @return: a DataFrame containing only the comments made in 2012-2013
+    """
+    start = pd.Timestamp(2012, 1, 1)
+    end = pd.Timestamp(2014, 1, 1)
+    return comments.query('@start <= date < @end')
 
 
 def separate_weekends_and_weekdays(data: pd.DataFrame):
@@ -14,3 +27,20 @@ def separate_weekends_and_weekdays(data: pd.DataFrame):
     weekday_comments = cpy[is_weekday_comment]['comment_count'].reset_index(drop=True)
     weekend_comments = cpy[~is_weekday_comment]['comment_count'].reset_index(drop=True)
     return weekday_comments, weekend_comments
+
+
+if not os.getenv('TESTING'):
+    comment_file = sys.argv[1]
+    reddit_comments = pd.read_json(comment_file, lines=True)
+    # filtered_comments = comments_only_in_2013_or_2014(reddit_comments)
+    # comments_in_2015 = reddit_comments['date'] > '2015-01-01'
+    # print(reddit_comments[comments_in_2015])
+    # wkday_comments, wkend_comments = separate_weekends_and_weekdays(reddit_comments)
+    # pval = stats.ttest_ind(wkend_comments, wkend_comments).pvalue
+    # print(pval)
+    # wkday_normality, wkend_normality = stats.normaltest(wkday_comments).pvalue, stats.normaltest(wkend_comments).pvalue
+    # print(wkday_normality)
+    # print(wkend_normality)
+    # equal_var = stats.levene(wkday_comments, wkend_comments)
+    # print(equal_var)
+
--- assignment-5/test_reddit_weekends.py
+++ assignment-5/test_reddit_weekends.py
@@ -17,3 +17,19 @@ actual_weekday_counts, actual_weekend_counts = rw.separate_weekends_and_weekdays
 def test_separate_weekends_and_weekdays():
     pdt.assert_series_equal(actual_weekend_counts, expected_weekend_counts, check_names=False)
     pdt.assert_series_equal(actual_weekday_counts, expected_weekday_counts, check_names=False)
+
+
+sample_comments = pd.DataFrame({"date": [pd.Timestamp(2024, 1, 9),
+                                         pd.Timestamp(2011, 8, 1),
+                                         pd.Timestamp(2013, 9, 4)],
+                                "subreddit": [1, 2, 3],
+                                "comment_count": [1, 9, 7]})
+
+expected_comments = pd.DataFrame({'date': [pd.Timestamp(2013, 9, 4)],
+                                  'subreddit': [3],
+                                  'comment_count': [7]})
+
+
+def test_comments_only_in_2012_or_2013():
+    pdt.assert_frame_equal(rw.comments_only_in_2012_or_2013(sample_comments).reset_index(drop=True),
+                           expected_comments)
