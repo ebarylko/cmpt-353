cb40619f170af1944572f0a3634b829f2d6d32db
Eitan Barylko <eitan@barylko.com>
2024-06-16 15:04:39 -0700

    Added a test for preparing the contingency table

--- assignment-6/ab_analysis.py
+++ assignment-6/ab_analysis.py
@@ -15,19 +15,23 @@ def count_of_users_who_did_and_did_not_search(sample_users: pd.DataFrame):
     return searched_more_than_once.index.size, never_searched.index.size
 
 
-# def prepare_contingency_table(searches: pd.DataFrame) -> pd.DataFrame:
-#     """
-#     @param searches: a DataFrame where each row contains a user id, boolean value indicating
-#     if the user is a teacher, the number of times a user logged in, and the number of times the
-#     user searched
-#     @return: a DataFrame containing two rows, the first representing the users with even ids and
-#     the second representing the users with odd ids. Each row contains the number of users who
-#     searched more than once and the number of users who never searched
-#     """
-#     has_even_id = searches['user_id'] % 2 == 0
-#     users_with_even_ids = searches[has_even_id]
-#     users_with_odd_ids = searches[~has_even_id]
-#
-#     even_id_searches, odd_id_searches = map(count_of_users_who_did_and_did_not_search,
-#                                             [users_with_even_ids, users_with_odd_ids])
-#
+def prepare_contingency_table(searches: pd.DataFrame) -> pd.DataFrame:
+    """
+    @param searches: a DataFrame where each row contains a user id, boolean value indicating
+    if the user is a teacher, the number of times a user logged in, and the number of times the
+    user searched
+    @return: a DataFrame containing two rows, the first representing the users with even ids and
+    the second representing the users with odd ids. Each row contains the number of users who
+    searched more than once and the number of users who never searched
+    """
+    has_even_id = searches['uid'] % 2 == 0
+    users_with_even_ids = searches[has_even_id]
+    users_with_odd_ids = searches[~has_even_id]
+
+    even_id_searches, odd_id_searches = map(count_of_users_who_did_and_did_not_search,
+                                            [users_with_even_ids, users_with_odd_ids])
+    searched_at_least_once, never_searched = list(zip(even_id_searches, odd_id_searches))
+    return pd.DataFrame({"searched_more_than_once": searched_at_least_once,
+                         "never_searched": never_searched},
+                        index=["even_id", "odd_id"])
+
--- assignment-6/test_ab_analysis.py
+++ assignment-6/test_ab_analysis.py
@@ -12,17 +12,17 @@ expected_search_values = (1, 1)
 
 def test_count_of_users_who_did_and_did_not_search():
     assert an.count_of_users_who_did_and_did_not_search(sample_users) == expected_search_values
-#
-#
-# sample_searches = pd.DataFrame({"uid": [1, 2, 3],
-#                                 "is_instructor": [True, False, True],
-#                                 "login_count": [1, 0, 9],
-#                                 "search_count": [2, 0, 4]})
-#
-# expected_table = pd.DataFrame({"searched_more_than_once": [0, 2],
-#                                "never_searched": [1, 0]},
-#                               index=["even_id", "odd_id"])
-#
-#
-# def test_prepare_contingency_table():
-#     pdt.assert_frame_equal(an.prepare_contingency_table(sample_searches), expected_table)
+
+
+sample_searches = pd.DataFrame({"uid": [1, 2, 3],
+                                "is_instructor": [True, False, True],
+                                "login_count": [1, 0, 9],
+                                "search_count": [2, 0, 4]})
+
+expected_table = pd.DataFrame({"searched_more_than_once": [0, 2],
+                               "never_searched": [1, 0]},
+                              index=["even_id", "odd_id"])
+
+
+def test_prepare_contingency_table():
+    pdt.assert_frame_equal(an.prepare_contingency_table(sample_searches), expected_table)
