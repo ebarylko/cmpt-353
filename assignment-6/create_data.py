2dbdbedb5e1dbe53111d102793b69bb67157ea7b
Eitan Barylko <eitan@barylko.com>
2024-06-19 16:44:48 -0700

    Changed how many datasets are generated

--- assignment-6/create_data.py
+++ assignment-6/create_data.py
@@ -13,7 +13,7 @@ def generate_datasets(num):
     @return: {num} number of datasets filled with random integers from -10,000 to 10,000
     """
     seed = default_rng()
-    return [seed.integers(-10000, high=10000, size=10000) for i in range(num)]
+    return [seed.integers(-70000, high=70000, size=30000) for i in range(num)]
 
 
 def get_runtime(f, dataset):
@@ -59,11 +59,10 @@ def prepare_runtime_chart(runtimes, column_names) -> pd.DataFrame:
     @return: a DataFrame where each column has the runtimes of a single algorithm on the provided datasets,
     and each column has the name of one of the provided datasets
     """
-    print(list(column_names))
-    # function_names_and_runtimes = dict(zip(column_names, runtimes))
-    # number_of_datasets = len(runtimes[0])
-    # dataset_names = ["dataset-{}".format(i + 1) for i in range(0, number_of_datasets)]
-    # return pd.DataFrame(function_names_and_runtimes, index=dataset_names)
+    function_names_and_runtimes = dict(zip(column_names, runtimes))
+    number_of_datasets = len(runtimes[0])
+    dataset_names = ["dataset-{}".format(i + 1) for i in range(0, number_of_datasets)]
+    return pd.DataFrame(function_names_and_runtimes, index=dataset_names)
 
 
 def get_func_name(f):
@@ -71,13 +70,10 @@ def get_func_name(f):
 
 
 if not os.getenv("TESTING"):
-    all_datasets = generate_datasets(10)
+    all_datasets = generate_datasets(50)
     function_runtimes = all_runtimes(all_datasets, all_implementations)
-    # print(all_implementations)
-    columns = list(map(get_func_name, all_implementations))
-    print(columns)
-    # print(list(columns))
-    # runtime_chart = prepare_runtime_chart(function_runtimes, columns)
-    # print(runtime_chart)
+    columns = map(get_func_name, all_implementations)
+    runtime_chart = prepare_runtime_chart(function_runtimes, columns)
+    print(runtime_chart)
 
 # print(list(all_runtimes(generate_datasets(10), all_implementations)))
