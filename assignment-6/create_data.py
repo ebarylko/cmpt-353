7d374cfffca7a25b98b0461bd5c0d8365ccfdc32
Eitan Barylko <eitan@barylko.com>
2024-06-19 16:35:46 -0700

    Added function which extracts the name of the function passes

--- assignment-6/create_data.py
+++ assignment-6/create_data.py
@@ -1,10 +1,22 @@
 from implementations import all_implementations
 from time import time
-from numpy.random import randint
+from numpy.random import default_rng
 import pandas as pd
+import functools as ft
+import os
+from operator import methodcaller
 
 
-def get_runtime(dataset, f):
+def generate_datasets(num):
+    """
+    @param num: the number of datasets wanted
+    @return: {num} number of datasets filled with random integers from -10,000 to 10,000
+    """
+    seed = default_rng()
+    return [seed.integers(-10000, high=10000, size=10000) for i in range(num)]
+
+
+def get_runtime(f, dataset):
     """
     @param dataset: a collection of integers
     @param f: the sorting function used to order the dataset
@@ -16,6 +28,29 @@ def get_runtime(dataset, f):
     return end - start
 
 
+def get_runtimes(datasets, f):
+    """
+    @param datasets: a collection of datasets where each one contains a large number of random integers
+    @param f: the sorting function to use on each dataset
+    @return: a collection of times, where each one represents the time it took to apply f on
+    a specific dataset
+    """
+    runtimes = ft.partial(get_runtime, f)
+    return list(map(runtimes, datasets))
+
+
+def all_runtimes(datasets, sorting_funcs):
+    """
+    @param datasets: a collection of datasets where each one contains a large number of random integers
+    @param f: the sorting function to use on each dataset
+    @param sorting_funcs: the sorting functions to benchmark
+    @return: a collection of collections, where each subcollection details the runtimes of a specific funtions
+    on each of the datasets
+    """
+    runtimes = ft.partial(get_runtimes, datasets)
+    return list(map(runtimes, sorting_funcs))
+
+
 def prepare_runtime_chart(runtimes, column_names) -> pd.DataFrame:
     """
     @param runtimes: a collection of collections, where each one contains the runtimes of the algorithms
@@ -24,7 +59,25 @@ def prepare_runtime_chart(runtimes, column_names) -> pd.DataFrame:
     @return: a DataFrame where each column has the runtimes of a single algorithm on the provided datasets,
     and each column has the name of one of the provided datasets
     """
-    function_names_and_runtimes = dict(zip(column_names, runtimes))
-    number_of_datasets = len(runtimes[0])
-    dataset_names = ["dataset-{}".format(i + 1) for i in range(0, number_of_datasets)]
-    return pd.DataFrame(function_names_and_runtimes, index=dataset_names)
+    print(list(column_names))
+    # function_names_and_runtimes = dict(zip(column_names, runtimes))
+    # number_of_datasets = len(runtimes[0])
+    # dataset_names = ["dataset-{}".format(i + 1) for i in range(0, number_of_datasets)]
+    # return pd.DataFrame(function_names_and_runtimes, index=dataset_names)
+
+
+def get_func_name(f):
+    return f.__name__
+
+
+if not os.getenv("TESTING"):
+    all_datasets = generate_datasets(10)
+    function_runtimes = all_runtimes(all_datasets, all_implementations)
+    # print(all_implementations)
+    columns = list(map(get_func_name, all_implementations))
+    print(columns)
+    # print(list(columns))
+    # runtime_chart = prepare_runtime_chart(function_runtimes, columns)
+    # print(runtime_chart)
+
+# print(list(all_runtimes(generate_datasets(10), all_implementations)))
