ac2fb2f4c9744b35d41d31fe7d66ee1388914f92
Eitan Barylko <eitan@barylko.com>
2024-06-21 18:22:40 -0700

    Removed plotting from analyse_data.py

--- assignment-6/analyse_data.py
+++ assignment-6/analyse_data.py
@@ -1,11 +1,8 @@
 import pandas as pd
 from statsmodels.stats.multicomp import pairwise_tukeyhsd
-from matplotlib import pyplot as plt
 
 
 data = pd.read_csv('data.csv')
 transformed_data = pd.melt(data)
 res = pairwise_tukeyhsd(transformed_data['value'], transformed_data['variable'])
 print(res)
-res.plot_simultaneous()
-plt.show()
