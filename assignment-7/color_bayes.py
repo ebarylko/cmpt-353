9a8891103cf55268fa9fb508e801d92fb5f04d69
Eitan Barylko <eitan@barylko.com>
2024-06-25 23:33:31 -0700

    Included the printing of the accuracy of the model on the validation data

--- assignment-7/color_bayes.py
+++ assignment-7/color_bayes.py
@@ -1,7 +1,11 @@
 import pandas as pd
+import os
+import sys
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
 
 
-def prepare_data_for_model(data: pd.DataFrame):
+def normalized_rgb_values_and_colors(data: pd.DataFrame):
     """
     @param data: a DataFrame where each row contains the R, G, and B value of a color, a guess for the name of the color
     corresponding to the RGB values, and an indication of how accurate the guess is
@@ -14,3 +18,13 @@ def prepare_data_for_model(data: pd.DataFrame):
     return rgb_values.to_numpy(), colors.to_numpy()
 
 
+if not os.getenv('TESTING'):
+    file_name = sys.argv[1]
+    color_data = pd.read_csv(file_name)
+
+    normalized_rgb_values, colors = normalized_rgb_values_and_colors(color_data)
+    training_rgb_values, validation_rgb_values, training_colors, validation_colors = train_test_split(normalized_rgb_values,
+                                                                                                      colors)
+    model = GaussianNB()
+    model.fit(training_rgb_values, training_colors)
+    print(model.score(validation_rgb_values, validation_colors))
