aaed669c0f09b201078127b426d386c9ec6179fd
Eitan Barylko <eitan@barylko.com>
2024-06-25 23:01:01 -0700

    Added a test for normalizing the rgb values

--- assignment-7/color_bayes.py
+++ assignment-7/color_bayes.py
@@ -1,3 +1,12 @@
 import pandas as pd
 
-pd.read_csv()
+
+def prepare_data_for_model(data: pd.DataFrame):
+    """
+    @param data: a DataFrame where each row contains the R, G, and B value of a color, a guess for the name of the color
+    corresponding to the RGB values, and an indication of how accurate the guess is
+    @return: a DataFrame containing the normalized RGB values and a Series containing the guesses for the colors
+    """
+    colors = data['Label']
+    rgb_values = data[['R', 'G', 'B']] / 255
+    return rgb_values, colors
--- assignment-7/test_color_bayes.py
+++ assignment-7/test_color_bayes.py
@@ -1,1 +1,25 @@
+import color_bayes as cb
 import pandas as pd
+import numpy as np
+import pandas.testing as pdt
+
+sample_data = pd.DataFrame({"R": [168, 37],
+                            "G": [211, 32],
+                            "B": [243, 40],
+                            "Label": ["blue", "black"],
+                            "Confidence": ["good", "horrible"]})
+
+
+# expected_colors = np.array([[168 / 255, 211 / 255, 243 / 255], [37 / 255, 32 / 255, 40 / 255]])
+expected_colors = pd.DataFrame({"R": [168 / 255, 37 / 255],
+                                "G": [211 / 255, 32 / 255],
+                                "B": [243 / 255, 40 / 255]})
+
+expected_labels = pd.Series(["blue", "black"])
+
+actual_colors, actual_labels = cb.prepare_data_for_model(sample_data)
+
+
+def test_prepare_data_for_model():
+    pdt.assert_frame_equal(expected_colors, actual_colors)
+    pdt.assert_series_equal(expected_labels, actual_labels, check_names=False)
