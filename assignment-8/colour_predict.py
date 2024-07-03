31a76d72df06df38bd543e9452995cd2e9af08a6
Eitan Barylko <eitan@barylko.com>
2024-07-03 13:55:56 -0700

    Changed how the models are generated

--- assignment-8/colour_predict.py
+++ assignment-8/colour_predict.py
@@ -8,6 +8,8 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.pipeline import make_pipeline
 from skimage.color import rgb2lab
+from functools import partial
+from itertools import chain
 
 
 def normalized_rgb_values_and_colors(data: pd.DataFrame):
@@ -55,6 +57,36 @@ OUTPUT_TEMPLATE = (
 )
 
 
+def create_both_models(model):
+    """
+    @param model: a machine learning model
+    @return: two instances of the model, one to be used on RGB values and the other to be used on
+    LAB colors
+    """
+    return model(), make_pipeline(FunctionTransformer(rgb2lab),
+                                  model())
+
+def train_and_evaluate_model(training_data, validation_data, model):
+    """
+    @param training_data: the data used to train the model
+    @param validation_data: the data used to validate the model
+    @param model: the model to be trained
+    @return: trains the model using training_data and evaluates it using the validation_data
+    """
+    model.fit(*training_data)
+    return model.score(*validation_data)
+
+
+# def get_validation_data_score(validation_data, model):
+#     """
+#     @param validation_data: the data used to validate the model
+#     @param model: the model to be used
+#     @return: the score of the model's predictions on the validation data
+#     """
+#     print(model)
+#     # print(model.score(*validation_data))
+#     return model.score(*validation_data)
+#
 if not os.getenv('TESTING'):
 
     file_name = sys.argv[1]
@@ -64,29 +96,24 @@ if not os.getenv('TESTING'):
     (training_rgb_values, validation_rgb_values,
      training_colors, validation_colors) = train_test_split(normalized_rgb_values,
                                                             colors)
-    naive_rgb_model = GaussianNB()
-    naive_rgb_model.fit(training_rgb_values, training_colors)
 
-    naive_lab_color_model = generate_bayes_lab_model()
-    naive_lab_color_model.fit(training_rgb_values, training_colors)
+    rgb_and_lab_models = chain.from_iterable(map(create_both_models,
+                                                 [GaussianNB, KNeighborsClassifier, DecisionTreeClassifier]
+                                                 ))
 
-    kneighbours_rgb_model = KNeighborsClassifier()
-    kneighbours_rgb_model.fit(training_rgb_values, training_colors)
+    fit_and_score = partial(train_and_evaluate_model,
+                       (training_rgb_values, training_colors),
+                       (validation_rgb_values, validation_colors))
 
-    kneighbours_lab_model = gen_neighbours_lab_model()
-    kneighbours_lab_model.fit(training_rgb_values, training_colors)
-
-    forest_rgb_model = DecisionTreeClassifier()
-    forest_rgb_model.fit(training_rgb_values, training_colors)
-
-    forest_lab_model = gen_decision_tree_lab_model()
-    forest_lab_model.fit(training_rgb_values, training_colors)
+    naive_rgb_score, naive_lab_score, neighbours_rgb_score, \
+        neighbours_lab_score, tree_rgb_score, tree_lab_score = map(fit_and_score, rgb_and_lab_models)
 
     print(OUTPUT_TEMPLATE.format(
-        bayes_rgb=naive_rgb_model.score(validation_rgb_values, validation_colors),
-        bayes_convert=naive_lab_color_model.score(validation_rgb_values, validation_colors),
-        knn_rgb=kneighbours_rgb_model.score(validation_rgb_values, validation_colors),
-        knn_convert=kneighbours_lab_model.score(validation_rgb_values, validation_colors),
-        rf_rgb=forest_rgb_model.score(validation_rgb_values, validation_colors),
-        rf_convert=forest_lab_model.score(validation_rgb_values, validation_colors),
+        bayes_rgb=naive_rgb_score,
+        bayes_convert=naive_lab_score,
+        knn_rgb=neighbours_rgb_score,
+        knn_convert=neighbours_lab_score,
+        rf_rgb=tree_rgb_score,
+        # rf_convert=forest_lab_model.score(training_rgb_values, training_colors)
+        rf_convert=tree_lab_score,
     ))
