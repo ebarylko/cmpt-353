6beaecd395eade3edfdb4502385e50dcc50dba85
Eitan Barylko <eitan@barylko.com>
2024-07-04 10:14:51 -0700

    Added a test for split_data

--- assignment-8/test_weather_city.py
+++ assignment-8/test_weather_city.py
@@ -1,2 +1,19 @@
 import weather_city as wc
+import pandas as pd
+import pandas.testing as pdt
 
+sample_data = pd.DataFrame({"city": ["A", "B"],
+                            "year": [1, 2],
+                            "snowfall": [4, 5]})
+
+expected_cities = pd.DataFrame({"city": ["A", "B"]})
+
+expected_year_and_snowfall = pd.DataFrame({"year": [1, 2],
+                                           "snowfall": [4, 5]})
+
+actual_cities, actual_year_and_snowfall = wc.split_data(sample_data, ['city'])
+
+
+def test_split_data():
+    pdt.assert_frame_equal(expected_cities, actual_cities)
+    pdt.assert_frame_equal(expected_year_and_snowfall, actual_year_and_snowfall)
--- assignment-8/weather_city.py
+++ assignment-8/weather_city.py
@@ -10,10 +10,27 @@ model = make_pipeline(
     RandomForestClassifier()
 )
 
-if not getenv('TESTING'):
-    labelled_file_name = argv[1]
 
-    labelled_data = pd.read_csv(labelled_file_name)
+def split_data(data: pd.DataFrame, columns_to_split_on):
+    """
+    @param data: a DataFrame containing data about the weather in multiple cities over a period of 55 years
+    @param columns_to_split_on: a subset of the columns in data
+    @return: two dataframes, the first containing the data associated with the columns in columns_to_split_on, and
+    the second containing the data pertaining to all the columns not in columns_to_split_on
+    """
+    cpy = data.copy()
+    is_part_of_columns_to_split_on = cpy.columns.isin(columns_to_split_on)
+    data_associated_with_columns = cpy.loc[:, is_part_of_columns_to_split_on]
+    data_not_associated_with_columns = cpy.loc[:, ~is_part_of_columns_to_split_on]
+    return data_associated_with_columns, data_not_associated_with_columns
 
-    months_and_weather, cities = split_data(labelled_data, ['city'])
-    print(labelled_data)
+
+
+# if not getenv('TESTING'):
+#     labelled_file_name = argv[1]
+#
+#     labelled_data = pd.read_csv(labelled_file_name)
+#
+#     months_and_weather, cities = split_data(labelled_data, ['city'])
+#     print(labelled_data)
+#
