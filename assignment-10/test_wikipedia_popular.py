e908d3ff55583932aa37adc0255563c58609299e
Eitan Barylko <eitan@barylko.com>
2024-07-17 23:43:29 -0700

    Added another test case

--- assignment-10/test_wikipedia_popular.py
+++ assignment-10/test_wikipedia_popular.py
@@ -1,9 +1,23 @@
 import wikipedia_popular as wp
+from pyspark.sql import SparkSession, types
+from chispa.column_comparer import assert_column_equality
+
+spark = SparkSession.builder.getOrCreate()
 
 sample_file1 = 'file:///Users/eitanbarylko/Workbench/cmpt-353/assignment-10/pagecounts-0/pagecounts-20160801-120000'
 sample_file2 = 'file:///Users/eitanbarylko/Workbench/cmpt-353/assignment-10/pagecounts-1/pagecounts-20160801-210000.gz'
+sample_file3 = 'file:///Users/eitanbarylko/Workbench/cmpt-353/assignment-10/pagecounts-1/pagecounts-20160802-220000.gz'
+
+filenames_and_expected_dates = [(sample_file1, '20160801-12'),
+                                (sample_file2, '20160801-21'),
+                                (sample_file3, '20160802-22'),]
+
+filename_expected_dates_and_actual_dates = (spark.createDataFrame(filenames_and_expected_dates, ['filename', 'expected_date']).
+                                            withColumn('actual_date', wp.filepath_to_date('filename')))
 
 
 def test_filepath_to_date():
-    assert wp.filepath_to_date(sample_file1) == '20160801-12'
-    assert wp.filepath_to_date(sample_file2) == '20160801-21'
+    assert_column_equality(filename_expected_dates_and_actual_dates, 'expected_date', 'actual_date')
+
+
+# def test_filter
--- assignment-10/wikipedia_popular.py
+++ assignment-10/wikipedia_popular.py
@@ -1,5 +1,5 @@
 from sys import argv, version_info
-from pyspark.sql import SparkSession, types, functions
+from pyspark.sql import SparkSession, types, functions, DataFrame
 from os import getenv
 import re
 
@@ -8,14 +8,9 @@ spark.sparkContext.setLogLevel('WARN')
 assert version_info >= (3, 8) # make sure we have Python 3.8+
 assert spark.version >= '3.2' # make sure we have Spark 3.2+
 
-wikipedia_page_schema = types.StructType([
-    types.StructField('language', types.StringType()),
-    types.StructField('page_title', types.StringType()),
-    types.StructField('times_requested', types.IntegerType()),
-    types.StructField('bytes', types.LongType())
-])
 
 
+@functions.udf(returnType=types.StringType())
 def filepath_to_date(filepath):
     """
     @param filepath: a path to a csv file containing pagecounts-YYYYMMDD-HHMMSS* as part of the name
@@ -25,11 +20,31 @@ def filepath_to_date(filepath):
     return date_of_file.groups()[0]
 
 
+def read_wikipedia_pages(pages_directory: str) -> DataFrame:
+    """
+    @param pages_directory: the name of a directory containing files where each contains rows having the language of the
+    wikipedia page accessed, the name of the page, the number of times it was accessed, and how many bytes were transmitted
+    @return: a DataFrame containing the previously mentioned information along with the date the page was accessed
+    """
+    wikipedia_page_schema = types.StructType([
+        types.StructField('language', types.StringType()),
+        types.StructField('page_title', types.StringType()),
+        types.StructField('times_requested', types.IntegerType()),
+        types.StructField('bytes', types.LongType())
+    ])
+
+    wikipedia_page_info = (spark.read.csv(pages_directory, sep=' ', schema=wikipedia_page_schema).
+                           withColumn('filepath', functions.input_file_name()))
+    page_info_with_date = wikipedia_page_info.withColumn('date', filepath_to_date('filepath'))
+    return page_info_with_date
+
 
 if not getenv('TESTING'):
     wikipedia_pages_directory = argv[1]
 
-    wikipedia_pages = spark.read.csv(wikipedia_pages_directory, sep=' ', schema=wikipedia_page_schema).withColumn('filename', functions.input_file_name())
-    names = wikipedia_pages.select('filename').limit(10).collect()
-    print(names)
+    wikipedia_pages = read_wikipedia_pages(wikipedia_pages_directory)
+
+
+    # names = wikipedia_pages.select('filename').limit(10).collect()
+    # print(names)
 
