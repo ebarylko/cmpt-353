841ccaa56b1cc204450f511b0e127e45f1aa586c
Eitan Barylko <eitan@barylko.com>
2024-07-23 11:03:55 -0700

    Added a test for extracting the hostname and number of bytes

--- assignment-11/correlate_logs.py
+++ assignment-11/correlate_logs.py
@@ -1,9 +1,24 @@
-from pyspark.sql import SparkSession, types, DataFrame, functions
+from pyspark.sql import SparkSession, types, DataFrame, functions, Row
 from os import getenv
+from re import match
 from sys import argv
 
 spark = SparkSession.builder.getOrCreate()
 
+LogInfo = Row("host", "bytes")
+
+
+def extract_hostname_and_bytes(log: str) -> LogInfo:
+    """
+    @param log: a log detailing an HTTP request sent to the NASA Kennedy space center
+    @return: a Row containing the hostname and the number of bytes sent in the request
+    """
+    hostname_and_bytes = r"^(\S+) - - \[\S+ [+-]\d+\] \"[A-Z]+ \S+ HTTP/\d\.\d\" \d+ (\d+)$"
+    search = match(hostname_and_bytes, log)
+    return None if search is None else LogInfo(search.group(1), int(search.group(2)))
+
+
 if not getenv('TESTING'):
     rows = spark.sparkContext.textFile("nasa-logs-1")
-    print(rows.collect())
+    print(rows.take(4))
+
--- assignment-11/test_correlate_logs.py
+++ assignment-11/test_correlate_logs.py
@@ -1,8 +1,24 @@
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession, Row
+from chispa.dataframe_comparer import  assert_df_equality
+import correlate_logs as cl
 
 spark = SparkSession.builder.getOrCreate()
 
-sample_data = spark.sparkContext.parallelize([""])
+# sample_logs = spark.sparkContext.parallelize(['199.72.81.55 - - [01/Jul/1995:00:00:01 -0400] "GET /history/apollo/ HTTP/1.0" 200 6245',
+#                                               'invalid text',
+#                                              'unicomp6.unicomp.net - - [01/Jul/1995:00:00:06 -0400] "GET /shuttle/countdown/ HTTP/1.0" 200 3985',
+#                                               '199.120.110.21 - - [01/Jul/1995:00:00:09 -0400] "GET /shuttle/missions/sts-73/mission-sts-73.html HTTP/1.0" 200 4085'
+#                                               ])
+
+# LogInfo = Row("host", "bytes")
+#
+# expected_hosts_and_bytes = spark.createDataFrame([LogInfo('199.72.81.55', 6245),
+#                                                   LogInfo(None, None),
+#                                                   LogInfo(None, None),
+#                                                   LogInfo('199.120.110.21', 4085)])
+
 
 def test_extract_hostname_and_bytes():
-
+    assert cl.LogInfo('199.72.81.55', 6245) == cl.extract_hostname_and_bytes('199.72.81.55 - - [01/Jul/1995:00:00:01 -0400] "GET /history/apollo/ HTTP/1.0" 200 6245')
+    assert cl.extract_hostname_and_bytes('invalid text') is None
+    assert cl.LogInfo('unicomp6.unicomp.net', 3985) == cl.extract_hostname_and_bytes('unicomp6.unicomp.net - - [01/Jul/1995:00:00:06 -0400] "GET /shuttle/countdown/ HTTP/1.0" 200 3985')
