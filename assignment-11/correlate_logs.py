0e419056feb416030688ef79e4a7afe731506d0e
Eitan Barylko <eitan@barylko.com>
2024-07-23 12:38:57 -0700

    Added a filter for invalid logs

--- assignment-11/correlate_logs.py
+++ assignment-11/correlate_logs.py
@@ -11,14 +11,25 @@ LogInfo = Row("host", "bytes")
 def extract_hostname_and_bytes(log: str) -> LogInfo:
     """
     @param log: a log detailing an HTTP request sent to the NASA Kennedy space center
-    @return: a Row containing the hostname and the number of bytes sent in the request
+    @return: a Row containing the hostname and the number of bytes sent in the request if the log is valid, false otherwise
     """
     hostname_and_bytes = r"^(\S+) - - \[\S+ [+-]\d+\] \"[A-Z]+ \S+ HTTP/\d\.\d\" \d+ (\d+)$"
     search = match(hostname_and_bytes, log)
     return None if search is None else LogInfo(search.group(1), int(search.group(2)))
 
 
+def is_valid_log(log: LogInfo) -> bool:
+    """
+    @param log: the log to validate
+    @return: true if the log is not None, false otherwise
+    """
+    return log is not None
+
+
 if not getenv('TESTING'):
     rows = spark.sparkContext.textFile("nasa-logs-1")
-    print(rows.take(4))
+
+    logs = rows.map(extract_hostname_and_bytes).filter(is_valid_log)
+    print(logs.take(10))
+
 
--- assignment-11/test_correlate_logs.py
+++ assignment-11/test_correlate_logs.py
@@ -1,24 +1,38 @@
+from pyspark.rdd import RDD
 from pyspark.sql import SparkSession, Row
-from chispa.dataframe_comparer import  assert_df_equality
 import correlate_logs as cl
 
 spark = SparkSession.builder.getOrCreate()
-
-# sample_logs = spark.sparkContext.parallelize(['199.72.81.55 - - [01/Jul/1995:00:00:01 -0400] "GET /history/apollo/ HTTP/1.0" 200 6245',
-#                                               'invalid text',
-#                                              'unicomp6.unicomp.net - - [01/Jul/1995:00:00:06 -0400] "GET /shuttle/countdown/ HTTP/1.0" 200 3985',
-#                                               '199.120.110.21 - - [01/Jul/1995:00:00:09 -0400] "GET /shuttle/missions/sts-73/mission-sts-73.html HTTP/1.0" 200 4085'
-#                                               ])
-
-# LogInfo = Row("host", "bytes")
-#
-# expected_hosts_and_bytes = spark.createDataFrame([LogInfo('199.72.81.55', 6245),
-#                                                   LogInfo(None, None),
-#                                                   LogInfo(None, None),
-#                                                   LogInfo('199.120.110.21', 4085)])
 
 
 def test_extract_hostname_and_bytes():
     assert cl.LogInfo('199.72.81.55', 6245) == cl.extract_hostname_and_bytes('199.72.81.55 - - [01/Jul/1995:00:00:01 -0400] "GET /history/apollo/ HTTP/1.0" 200 6245')
     assert cl.extract_hostname_and_bytes('invalid text') is None
     assert cl.LogInfo('unicomp6.unicomp.net', 3985) == cl.extract_hostname_and_bytes('unicomp6.unicomp.net - - [01/Jul/1995:00:00:06 -0400] "GET /shuttle/countdown/ HTTP/1.0" 200 3985')
+
+
+def is_valid_log(log):
+    return log is not None
+
+
+sample_data = spark.sparkContext.parallelize([cl.LogInfo('199.72.81.55', 6245),
+                                              None,
+                                              None,
+                                              cl.LogInfo('199.120.110.21', 4085)])
+
+actual_logs = sample_data.filter(is_valid_log)
+
+expected_logs = spark.sparkContext.parallelize([cl.LogInfo('199.72.81.55', 6245),
+                                                cl.LogInfo('199.120.110.21', 4085)])
+
+
+def logs_match(log1: cl.LogInfo, log2: cl.LogInfo):
+    return log1['host'] == log2['host'] and log1['bytes'] == log2['bytes']
+
+
+def assert_logs_equal(expected, actual):
+    return all(map(logs_match, expected.collect(), actual.collect()))
+
+
+def test_filter_valid_logs():
+    assert assert_logs_equal(expected_logs, actual_logs)
